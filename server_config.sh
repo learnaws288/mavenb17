27e2ea5ec5926aed8d1b678f59c73f266936664c
harshagowda09 <149284703+harshagowda09@users.noreply.github.com>
2023-11-18 09:57:37 +0530

    Update server_config.sh

--- server_config.sh
+++ server_config.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-sudo yum install java-1.8.0-openjdk-devel -y
-sudo yum install git -y
-sudo yum install maven -y
+sudo apt update -y
+sudo apt install default-jre -y
+sudo apt install maven -y
